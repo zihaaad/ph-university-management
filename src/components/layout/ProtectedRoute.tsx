0a4379d204580aafe91e00310866be280359e15f
Zihadul Islam <zihadislam2429@gmail.com>
2024-03-02 09:04:11 +0600

    Role based authentication [handled]

--- src/components/layout/ProtectedRoute.tsx
+++ src/components/layout/ProtectedRoute.tsx
@@ -1,10 +1,33 @@
 import {ReactNode} from "react";
-import {useAppSelector} from "../../redux/hooks";
-import {selectCurrentToken} from "../../redux/features/auth/authSlice";
+import {useAppDispatch, useAppSelector} from "../../redux/hooks";
+import {
+  logout,
+  selectCurrentToken,
+  selectCurrentUser,
+} from "../../redux/features/auth/authSlice";
 import {Navigate} from "react-router-dom";
+import {verifyToken} from "../../utils/verifyToken";
 
-const ProtectedRoute = ({children}: {children: ReactNode}) => {
+type TProtectedRouteProps = {
+  children: ReactNode;
+  role: string | undefined;
+};
+
+const ProtectedRoute = ({children, role}: TProtectedRouteProps) => {
+  const dispatch = useAppDispatch();
+
   const token = useAppSelector(selectCurrentToken);
+
+  let user;
+  if (token) {
+    user = verifyToken(token as string);
+  }
+
+  if (role !== undefined && role !== user?.role) {
+    dispatch(logout());
+    return <Navigate to={"/login"} replace={true} />;
+  }
+
   if (!token) {
     return <Navigate to={"/login"} replace={true} />;
   }
--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -6,7 +6,8 @@ import {adminPaths} from "../../routes/admin.routes";
 import {facultyPaths} from "../../routes/faculty.routes";
 import {studentPaths} from "../../routes/student.routes";
 import {useAppSelector} from "../../redux/hooks";
-import {selectCurrentUser} from "../../redux/features/auth/authSlice";
+import {TUser, selectCurrentToken} from "../../redux/features/auth/authSlice";
+import {verifyToken} from "../../utils/verifyToken";
 
 const userRole = {
   ADMIN: "admin",
@@ -15,10 +16,15 @@ const userRole = {
 };
 
 const Sidebar = () => {
-  const user = useAppSelector(selectCurrentUser);
+  const token = useAppSelector(selectCurrentToken);
+
+  let user;
+  if (token) {
+    user = verifyToken(token as string);
+  }
   let sidebarItems;
 
-  switch (user!.role) {
+  switch ((user as TUser)!.role) {
     case userRole.ADMIN:
       sidebarItems = sidebarItemsGenerator(adminPaths, userRole.ADMIN);
       break;
